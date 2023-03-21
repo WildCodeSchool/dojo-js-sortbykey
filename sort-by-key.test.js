df0725f56831d19087e531c02e7fb8fbac303737
YDrogen <13053644+YDrogen@users.noreply.github.com>
2023-03-21 09:08:25 +0100

    Initial commit

---
+++ sort-by-key.test.js
@@ -0,0 +1,36 @@
+const assert = require("assert");
+const sortByKey = require("./sort-by-key");
+
+const users = [
+  { name: "Charlie", age: 38 },
+  { name: "Bob", age: 19 },
+  { name: "Alice", age: 32 },
+];
+
+describe("Sort By Key", () => {
+  it("should be a function", () => {
+    assert.equal(typeof sortByKey, "function");
+  });
+
+  it("should return an array", () => {
+    assert.equal(Array.isArray(sortByKey([])), true);
+  });
+
+  it("should sort by name alphabetically", () => {
+    const orderedByName = [
+      { name: "Alice", age: 32 },
+      { name: "Bob", age: 19 },
+      { name: "Charlie", age: 38 },
+    ];
+    assert.deepEqual(sortByKey(users), orderedByName);
+  });
+
+  it("should order by increasing age", () => {
+    const orderedByAge = [
+      { name: "Bob", age: 19 },
+      { name: "Alice", age: 32 },
+      { name: "Charlie", age: 38 },
+    ];
+    assert.deepEqual(sortByKey(users, "age"), orderedByAge);
+  });
+});
